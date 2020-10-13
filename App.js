8b92a24393b7e0320333dd03a954252808421dae
bachhs <bachhs21@gmail.com>
2020-10-13 21:39:59 +0700

    add login feature

--- App.js
+++ App.js
@@ -1,25 +1,46 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text } from 'react-native';
+import auth from '@react-native-firebase/auth';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
-
-const Stack = createStackNavigator();
+import LoginScreen from './src/screens/LoginScreen/LoginScreen'
+import RegistrationScreen from './src/screens/RegistrationScreen/RegistrationScreen'
 
 function App() {
+  // Set an initializing state whilst Firebase connects
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
+
+  // Handle user state changes
+  function onAuthStateChanged(user) {
+    setUser(user);
+    if (initializing) setInitializing(false);
+  }
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
+  }, []);
+
+  if (initializing) return null;
+
+  const Stack = createStackNavigator();
+
+  if (!user) {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Login">
+          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="Registration" component={RegistrationScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View>
+      <Text>Welcome {user.email}</Text>
+    </View>
   );
 }
 
---
+++ src/screens/LoginScreen/LoginScreen.js
@@ -0,0 +1,81 @@
+import React, { useState } from 'react'
+import { Image, Text, TextInput, Button, View } from 'react-native'
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import styles from './styles';
+import auth from '@react-native-firebase/auth';
+
+export default function LoginScreen({ navigation }) {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const onFooterLinkPress = () => {
+        navigation.navigate('Registration')
+    }
+
+    const onLoginPress = () => {
+        auth()
+            .signInWithEmailAndPassword(email, password)
+            .then((response) => {
+                const uid = response.user.uid
+                const usersRef = firestore().collection('users')
+                usersRef
+                    .doc(uid)
+                    .get()
+                    .then(firestoreDocument => {
+                        if (!firestoreDocument.exists) {
+                            alert("User does not exist anymore.")
+                            return;
+                        }
+                        const user = firestoreDocument.data()
+                        navigation.navigate('Home', { user: user })
+                    })
+                    .catch(error => {
+                        alert(error);
+                    });
+            })
+            .catch(error => {
+                alert(error)
+            })
+    }
+
+    return (
+        <View style={styles.container}>
+            <KeyboardAwareScrollView
+                style={{ flex: 1, width: '100%' }}
+                keyboardShouldPersistTaps="always">
+                <Image
+                    style={styles.logo}
+                    source={require('../../../assets/icon.png')}
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholder='E-mail'
+                    placeholderTextColor="#aaaaaa"
+                    onChangeText={(text) => setEmail(text)}
+                    value={email}
+                    underlineColorAndroid="transparent"
+                    autoCapitalize="none"
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholderTextColor="#aaaaaa"
+                    secureTextEntry
+                    placeholder='Password'
+                    onChangeText={(text) => setPassword(text)}
+                    value={password}
+                    underlineColorAndroid="transparent"
+                    autoCapitalize="none"
+                />
+                <Button
+                    title="Login"
+                    style={styles.button}
+                    onPress={() => onLoginPress()}>
+                    <Text style={styles.buttonTitle}>Log in</Text>
+                </Button>
+                <View style={styles.footerView}>
+                    <Text style={styles.footerText}>Don't have an account? <Text onPress={onFooterLinkPress} style={styles.footerLink}>Sign up</Text></Text>
+                </View>
+            </KeyboardAwareScrollView>
+        </View>
+    )
+}
