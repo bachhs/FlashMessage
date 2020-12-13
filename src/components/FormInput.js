56a96cef9b404522316c59c09da460afd37007e4
bachhs <bachhs21@gmail.com>
2020-12-13 22:17:09 +0700

    add color for text input
    Signed-off-by: bachhs <bachhs21@gmail.com>

--- src/components/FormInput.js
+++ src/components/FormInput.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import { TextInput } from 'react-native-paper';
 import styles from './styles';
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
+const theme = {
+  ...DefaultTheme,
+  roundness: 2,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#1976d2',
+    accent: '#f1c40f',
+  },
+};
 
 export default function FormInput({ labelName, ...rest }) {
   return (
@@ -8,6 +18,7 @@ export default function FormInput({ labelName, ...rest }) {
       label={labelName}
       style={styles.input}
       numberOfLines={1}
+      theme={theme}
       {...rest}
     />
   );
